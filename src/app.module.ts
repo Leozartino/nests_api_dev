040b4e932dc2f4424edf040d43052bf064215281
leozartino <leozartino@gmail.com>
2021-08-29 18:40:38 -0300

    refactor: remoing template controllers & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { CoursesService } from './courses/courses.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
+  controllers: [CoursesController],
+  providers: [CoursesService],
 })
 export class AppModule {}
