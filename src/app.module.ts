79f00700b2394fe83eac984081a26193aed3c5da
leozartino <leozartino@gmail.com>
2021-08-29 20:30:09 -0300

    feat: add typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'leonardo',
+      password: 'postgres',
+      database: 'estudo_nestjs',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
