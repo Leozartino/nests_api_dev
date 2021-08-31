75059067e9746dc87cbde0fb9c1f520262b1a0ca
leozartino <leozartino@gmail.com>
2021-08-31 17:21:45 -0300

    feat: add infectable service tag in courses MtoM

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -3,13 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCourseDTO, UpdateCourseDTO } from 'src/Dtos/course.dto';
 import { Course } from 'src/entities/course.entity';
 import { Repository } from 'typeorm';
-
+import { Tag } from 'src/entities/tag.entity';
 //bussines rule
 @Injectable()
 export class CoursesService {
   public constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
   public async findAll(): Promise<Course[]> {
     return await this.courseRepository.find();
@@ -24,7 +26,15 @@ export class CoursesService {
   }
 
   public async create(createCourseDTO: CreateCourseDTO): Promise<Course> {
-    const newCourse: Course = this.courseRepository.create(createCourseDTO);
+    const tags = await Promise.all(
+      createCourseDTO.tags.map((tag: Tag) =>
+        this.preLoadTagByName(tag.name_tag),
+      ),
+    );
+    const newCourse: Course = this.courseRepository.create({
+      ...createCourseDTO,
+      tags,
+    });
     return await this.courseRepository.save(newCourse);
   }
 
@@ -33,9 +43,17 @@ export class CoursesService {
     updateCourseDTO: UpdateCourseDTO,
   ): Promise<Course> {
     try {
+      const tags =
+        updateCourseDTO.tags &&
+        (await Promise.all(
+          updateCourseDTO.tags.map((tag: Tag) =>
+            this.preLoadTagByName(tag.name_tag),
+          ),
+        ));
       const courseUpdated: Course = await this.courseRepository.preload({
         id: Number(id),
         ...updateCourseDTO,
+        tags,
       });
       const result: Course = await this.courseRepository.save(courseUpdated);
 
@@ -55,5 +73,14 @@ export class CoursesService {
     } catch (error) {
       throw new HttpException(`Course doesn't exist`, HttpStatus.NOT_FOUND);
     }
+  }
+  private async preLoadTagByName(name_tag: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ name_tag });
+
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagRepository.create({ name_tag });
   }
 }
